2623400757fa440ab306befda8530c372b921977
Lwin Moe Paing <lwinmoepaing.dev@gmail.com>
2023-12-28 00:14:02 +0630

    fix(api): update const 404 status code

--- apps/server/api/_404.ts
+++ apps/server/api/_404.ts
@@ -1,4 +1,5 @@
 import type { IncomingMessage, ServerResponse } from "node:http";
+import { StatusCodes } from "http-status-codes";
 import { successResponse } from "../util/response";
 
 export default function notFoundHandler(
@@ -6,5 +7,5 @@ export default function notFoundHandler(
   res: ServerResponse
 ) {
   res.setHeader("Content-Type", "application/json");
-  res.end(JSON.stringify(successResponse(404, "Not Found")));
+  res.end(JSON.stringify(successResponse(StatusCodes.NOT_FOUND, "Not Found")));
 }
